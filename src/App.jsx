c2fa1c21ad256caae85c379233f87c2284cef151
SilenceCr1stal <nikitapolyashov2@gmail.com>
2024-10-03 22:29:03 +0300

    added modal window v1.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,26 @@
 import React, { useState } from 'react'
 
 function App() {
+  
 
-  const [count, setCount] = useState(0)
-
-  return <React.Fragment>
-    <div className='App'>
+  return (
+    <div className="App">
       <div className="wrap">
-        <h1 className="App__header">Счетчик</h1>
-        <h2 className="App__counter">{count}</h2>
-        <div className="App__btns">
-          <button className="btn btn__inc" onClick={() => setCount(prev => prev += 1)}>+ Плюс</button>
-          <button className="btn btn__dec" onClick={() => setCount(prev => prev > 0 ? prev -= 1 : 0)}>- Минус</button>
+        <div className="modal">
+          <h1 className='modal__header'><span>&#127775;</span> Открыть окно</h1>
         </div>
+        {/* <div className="overlay">
+          <div className="overlay__wrap">
+            <svg height="200" viewBox="0 0 200 200" width="200">
+              <title />
+              <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+            </svg>
+            <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" alt='GIF'/>
+          </div>
+        </div> */}
       </div>
     </div>
-  </React.Fragment>
+  )
 }
 
 export default App
