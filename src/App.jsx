0b80558df9970593db61dace795d50089978e9f1
SilenceCr1stal <nikitapolyashov2@gmail.com>
2024-10-03 22:36:36 +0300

    added modal window v1.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,36 @@
 import React, { useState } from 'react'
 
 function App() {
-  
+
+  function onClickOpen() {
+    setOpen(prev => prev = !prev)
+  }
+
+  const [open, setOpen] = useState(false)
 
   return (
     <div className="App">
       <div className="wrap">
-        <div className="modal">
-          <h1 className='modal__header'><span>&#127775;</span> Открыть окно</h1>
-        </div>
-        {/* <div className="overlay">
-          <div className="overlay__wrap">
-            <svg height="200" viewBox="0 0 200 200" width="200">
-              <title />
-              <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
-            </svg>
-            <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" alt='GIF'/>
-          </div>
-        </div> */}
+        {
+          !open && (
+            <div className="modal">
+              <h1 className='modal__header' onClick={onClickOpen}><span>&#127775;</span> Открыть окно</h1>
+            </div>
+          )
+        }
+        {
+          open && (
+            <div className="overlay">
+              <div className="overlay__wrap">
+                <svg height="200" viewBox="0 0 200 200" width="200" onClick={onClickOpen}>
+                  <title />
+                  <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+                </svg>
+                <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" alt='GIF' />
+              </div>
+            </div>
+          )
+        }
       </div>
     </div>
   )
