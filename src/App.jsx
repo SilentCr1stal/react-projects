a57cb8e08f42c7f44bf02d2caf0acbfeb03e2e95
SilenceCr1stal <nikitapolyashov2@gmail.com>
2024-10-05 00:05:27 +0300

    added modal window v1.2

--- src/App.jsx
+++ src/App.jsx
@@ -22,11 +22,13 @@ function App() {
           open && (
             <div className="overlay">
               <div className="overlay__wrap">
-                <svg height="200" viewBox="0 0 200 200" width="200" onClick={onClickOpen}>
-                  <title />
-                  <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
-                </svg>
-                <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" alt='GIF' />
+                <div className="overlay__block">
+                  <svg height="200" viewBox="0 0 200 200" width="200" onClick={onClickOpen}>
+                    <title />
+                    <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+                  </svg>
+                  <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" alt='GIF' />
+                </div>
               </div>
             </div>
           )
