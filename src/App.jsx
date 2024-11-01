6418be77efcae75cd5b3249d5bce2a1e1a666426
SilenceCr1stal <nikitapolyashov2@gmail.com>
2024-11-01 15:39:33 +0300

    currency converter init

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
-
+import React from 'react';
+import { Block } from './Block';
 
 function App() {
+  const [rates, setRates] = React.useState([])
+
+  React.useEffect(() => {
+    try {
+      const req = fetch('http://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/currency')
+      .then(res => res.json())
+      console.log(req);
+      
+    } catch (error) {
+      console.error('Error - ', error)
+    }
+  })
+
   return (
     <div className="App">
-      
+      <Block value={0} currency="RUB" onChangeCurrency={(cur) => console.log(cur)} />
+      <Block value={0} currency="USD" />
     </div>
-  )
+  );
 }
+
+export default App;
